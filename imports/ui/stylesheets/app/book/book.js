d4fc8fb0c71aa888f3f9fe5cc0f90e1af5fa9404
askesis <asketes@milkandcartoons.com>
2018-03-03 23:15:47 +0300

    Add some style

---
+++ imports/ui/stylesheets/app/book/book.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const book = StyleSheet.create({
+  container: {
+    width: '100%'
+  },
+  items: {
+    padding: 10,
+    marginLeft: 16
+  },
+  itemText: {
+    fontSize: 14,
+    height: 25,
+  }
+});
+
+export default book;
+
+
+
+
---
+++ imports/ui/stylesheets/app/common.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+const common = StyleSheet.create({
+  full_width: {
+    width: '100%'
+  },
+});
+
+export default common;
