d33555c91ae5d63480d3fa5ca01aa5a93f4b33c7
askesis <asketes@milkandcartoons.com>
2018-03-11 16:35:11 +0300

    Update style

--- imports/ui/stylesheets/app/layout.js
+++ imports/ui/stylesheets/app/layout.js
@@ -2,7 +2,6 @@ import { StyleSheet } from 'react-native';
 
 const layout = StyleSheet.create({
   container: {
-    marginTop: 20,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
