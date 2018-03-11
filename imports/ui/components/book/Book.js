4cdc19dcbd67d122c0471a78f6a684ec7a277245
askesis <asketes@milkandcartoons.com>
2018-03-11 16:37:15 +0300

    Make Touchable Book on the book list

--- imports/ui/components/book/Book.js
+++ imports/ui/components/book/Book.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { Card, ListItem, Button } from 'react-native-elements'
 
 import book from '../../stylesheets/app/book/book';
@@ -7,13 +7,34 @@ import book from '../../stylesheets/app/book/book';
 const avatar = 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg';
 
 class Book extends React.Component {
+
+  constructor(props) {
+    super(props);
+    
+    this.onClickCard = this.onClickCard.bind(this);
+    this.pushPage = this.pushPage.bind(this);
+  }
+
+  pushPage(){
+    this.props.navigate('Book');
+  }
+
+  onClickCard(){
+    this.pushPage();
+  }
+  
   render() {
     const { item } = this.props;
-    return (
-      <Card image={ require('../../img/kanada-banf-gory.jpg') } >
-        <Text style={book.text}>{item.name}, <Text >{item.author}</Text></Text>
-        <Text style={book.range}>{item.range}</Text>
-      </Card>
+
+    return ( 
+      <View>
+        <TouchableOpacity activeOpacity={ .7 } onPress={this.onClickCard} >
+          <Card image={ require('../../img/kanada-banf-gory.jpg') } >
+            <Text style={book.text}>{item.name}, <Text>{item.author}</Text></Text>
+            <Text style={book.range}>{item.range}</Text>
+          </Card>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
